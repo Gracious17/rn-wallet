69be1658f073f7b1afaafaa274f482839c10898b
Gracious17 <kingsleygracious16@gmail.com>
2026-01-05 16:33:30 +0100

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+// middleware 
+app.use(rateLimiter);
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+
+app.use("/api/transactions", transactionsRoute);
+// start the server after initializing the
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is up and running on PORT: ${PORT}`);
+  });
+});
